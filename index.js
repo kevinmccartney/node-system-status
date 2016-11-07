ecf51f92fda078d9f86fd2f25067bd02bb5366ff
Kevin McCartney <hello@kevinmccartney.is>
2016-11-06 20:29:52 -0500

    Added uptime

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const util = require('util');
+const os = require('os');
 const console = require('console');
 const moment = require('moment');
 
@@ -5,8 +7,22 @@ const time = () => {
     return 'Report generated at ' + moment().format('MMMM Do YYYY, H:mm:ssa');
 };
 
+const uptime = () => {
+    let duration = moment.duration(os.uptime(), 'seconds');
+    let units = ['days', 'hours', 'minutes', 'seconds'];
+
+    let uptime = units.map(unit => {
+        return duration.get(unit) + ' ' + unit;
+    });
+
+    return uptime;
+}
+
 const main = () => {
-    return time();
+    return {
+        "uptime": uptime(),
+        "time": time()
+    };
 };
 
-console.log(main());
+console.log(util.inspect(main()));
